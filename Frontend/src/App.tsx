343d5e130b0b75dcc4e81c06d3550b5169382cde
garv108 <garvpandya2@gmail.com>
2026-01-08 05:45:49 +0530

    Fix: Import Notes component in App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
+import Notes from "./pages/notes";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { AuthProvider } from "./lib/auth";
 import Home from "./pages/home";
